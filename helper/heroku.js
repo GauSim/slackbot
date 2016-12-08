f96a3a9977afa81183f1e689cf6de01e34b49b3c
Simon Gausmann <Simon.Gausmann@live.de>
2016-12-08 20:17:20 +0100

    https is not supported

--- helper/heroku.js
+++ helper/heroku.js
@@ -1,11 +1,21 @@
 const http = require("http");
 
 function keepAlive() {
-    
-    console.log(`starting loop ${Date.now()} ${process.env.redirectUri}`);
+
+    console.log(`starting loop ${Date.now()} ${process.env.pingURL}`);
     setInterval(function () {
-        console.log(`sending ping ${Date.now()} ${process.env.redirectUri}`);
-        http.get(process.env.redirectUri);
+        console.log(`sending ping ${Date.now()} ${process.env.pingURL}`);
+
+        try {
+            if (process.env.pingURL) {
+                http.get(process.env.pingURL);
+            } else {
+                console.error('process.env.pingURL missing');
+            }
+        } catch (ex) {
+            console.error(er);
+        }
+
     }, 300000); // every 5 minutes (300000)
 }
 
