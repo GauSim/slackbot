cfa19bd031d3f65b4d4e313133df8cd0a9e4a4c2
Simon Gausmann <Simon.Gausmann@CoreSystems.ch>
2018-05-30 13:15:57 +0200

    [task] tar.gz no support by runting O_o

--- createArtefact.sh
+++ createArtefact.sh
@@ -5,13 +5,14 @@ set -x
 
 timestamp=$(awk 'BEGIN {srand(); print srand()}');
 gitRev="$(git rev-parse HEAD)";
-filename="slackbot#$gitRev@$timestamp.tar.gz";
+
 
 # echo "clean all node modules";
 # rm -rf node_modules;
 
 echo "clean artefacts";
 rm -f *.tar.gz;
+rm -f *.zip;
 
 echo "install dependency";
 npm install;
@@ -22,8 +23,11 @@ npm run build;
 echo "remove .env";
 npm run clean:dotenv;
 
+#filename="slackbot#$gitRev@$timestamp.tar.gz";
+filename="slackbot#$gitRev@$timestamp.zip";
 echo "creat zip file artefact ($filename)";
-touch $filename
-tar --exclude=$filename -zcvf $filename .
+zip -r $filename *;
+#touch $filename
+#tar --exclude=$filename --exclude=".*" -zcvf $filename .
 
 echo "done => $filename";
