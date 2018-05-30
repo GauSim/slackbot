ed5a660cd32de2490da2c7ac8f35755483760c75
Simon Gausmann <Simon.Gausmann@CoreSystems.ch>
2018-05-30 12:06:51 +0200

    build tooling

--- createArtefact.sh
+++ createArtefact.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+set -e
+set -x
+
 timestamp=$(awk 'BEGIN {srand(); print srand()}');
 gitRev="$(git rev-parse HEAD)";
-filename="slackbot#$gitRev@$timestamp.zip";
+filename="slackbot#$gitRev@$timestamp.tar.gz";
 
-
-
-echo "clean all node modules";
-#rm -rf node_modules;
+# echo "clean all node modules";
+# rm -rf node_modules;
 
 echo "clean artefacts";
-rm *.zip;
+rm -f *.tar.gz;
 
 echo "install dependency";
 npm install;
@@ -22,6 +23,6 @@ echo "remove .env";
 npm run clean:dotenv;
 
 echo "creat zip file artefact ($filename)";
-zip -r $filename *;
+tar -zcvf $filename .
 
 echo "done => $filename";
